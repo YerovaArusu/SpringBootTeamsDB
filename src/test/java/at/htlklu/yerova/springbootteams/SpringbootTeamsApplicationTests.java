ac8acf549b2c8ffa2ee14801343f1aab629756dd
Kalvin <Kalvin.nemec@edu.htl-klu.at>
2024-12-17 08:43:47 +0100

    - init - setup packages

---
+++ src/test/java/at/htlklu/yerova/springbootteams/SpringbootTeamsApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlklu.yerova.springbootteams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTeamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
